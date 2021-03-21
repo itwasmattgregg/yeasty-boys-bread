75657bae96bec8a3bcdf12d6fa9725b0575e0f6a
Matt Gregg <mgregg@nerdery.com>
2021-03-21 18:49:25 -0500

    Initial commit. Working form

---
+++ pages/api/addRequest.js
@@ -0,0 +1,41 @@
+import { connectToDatabase } from "../../util/mongodb";
+
+export default async (req, res) => {
+
+
+  if (req.method === 'POST') {
+    const { db } = await connectToDatabase();
+
+    // Check if user is already in database
+    const findEmail = await db
+      .collection("sourdough")
+      .findOne({
+        email: req.body.email
+      });
+
+    if (findEmail) {
+      res.status(500).send("You've already been added to the list");
+      return;
+    }
+
+    // add request to database
+    const request = await db
+      .collection("sourdough")
+      .insertOne({
+        ...req.body,
+        numberOfBreads: 0,
+      });
+
+    res.status(201).json(request);
+  } else {
+    res.status(405).end();
+  }
+
+  // const requests = await db
+  //   .collection("sourdough")
+  //   .find({})
+  //   .limit(20)
+  //   .toArray();
+
+  // res.json(request);
+};
