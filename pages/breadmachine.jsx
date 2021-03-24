9dcf259724e172b981d209ad2c2b8eb12554dba0
Matt Gregg <mgregg@nerdery.com>
2021-03-23 19:50:08 -0500

    beastie boys sounds

--- pages/breadmachine.jsx
+++ pages/breadmachine.jsx
@@ -53,46 +53,46 @@ export default function BreadMachine() {
           <div className="keys">
             <div data-key="65" className="key">
               <kbd>A</kbd>
-              <span className="sound">clap</span>
+              <span className="sound">Guitar 1</span>
             </div>
             <div data-key="83" className="key">
               <kbd>S</kbd>
-              <span className="sound">hihat</span>
+              <span className="sound">Guitar 2</span>
             </div>
             <div data-key="68" className="key">
               <kbd>D</kbd>
-              <span className="sound">kick</span>
+              <span className="sound">Guitar 3</span>
             </div>
             <div data-key="70" className="key">
               <kbd>F</kbd>
-              <span className="sound">openhat</span>
+              <span className="sound">Guitar 4</span>
             </div>
             <div data-key="71" className="key">
               <kbd>G</kbd>
-              <span className="sound">boom</span>
+              <span className="sound">Kick</span>
             </div>
             <div data-key="72" className="key">
               <kbd>H</kbd>
-              <span className="sound">ride</span>
+              <span className="sound">No Sleep</span>
             </div>
             <div data-key="74" className="key">
               <kbd>J</kbd>
-              <span className="sound">snare</span>
+              <span className="sound">Brooklyn</span>
             </div>
             <div data-key="75" className="key">
               <kbd>K</kbd>
-              <span className="sound">tom</span>
+              <span className="sound">Snare</span>
             </div>
           </div>
 
-          <audio data-key="65" src="sounds/clap.wav"></audio>
-          <audio data-key="83" src="sounds/hihat.wav"></audio>
-          <audio data-key="68" src="sounds/kick.wav"></audio>
-          <audio data-key="70" src="sounds/openhat.wav"></audio>
-          <audio data-key="71" src="sounds/boom.wav"></audio>
-          <audio data-key="72" src="sounds/ride.wav"></audio>
-          <audio data-key="74" src="sounds/snare.wav"></audio>
-          <audio data-key="75" src="sounds/tom.wav"></audio>
+          <audio data-key="65" src="sounds/guitar1.wav"></audio>
+          <audio data-key="83" src="sounds/guitar2.wav"></audio>
+          <audio data-key="68" src="sounds/guitar3.wav"></audio>
+          <audio data-key="70" src="sounds/guitar4.wav"></audio>
+          <audio data-key="71" src="sounds/kick.wav"></audio>
+          <audio data-key="72" src="sounds/nosleep.wav"></audio>
+          <audio data-key="74" src="sounds/brooklyn.wav"></audio>
+          <audio data-key="75" src="sounds/snare.wav"></audio>
         </main>
         <style jsx>{`
           .keys {
