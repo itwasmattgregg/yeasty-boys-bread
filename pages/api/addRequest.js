e38af18d93c7462249a1bf3cd22f0b5bdd0da83d
Matt Gregg <mattdgregg@gmail.com>
2021-04-18 11:15:27 -0500

    add sendgrid list capability

--- pages/api/addRequest.js
+++ pages/api/addRequest.js
@@ -1,30 +1,43 @@
 import { connectToDatabase } from '../../util/mongodb';
+const client = require('@sendgrid/client');
 
 export default async (req, res) => {
   if (req.method === 'POST') {
     const { db } = await connectToDatabase();
 
-    let regex = /\+(.*)(?=@)/gm;
-    const strippedEmail = req.body.email.replace(regex, '');
+    client.setApiKey(process.env.SENDGRID_API_KEY);
+    const request = {
+      method: 'PUT',
+      url: '/v3/marketing/contacts',
+    };
 
-    // add request to database
-    let response;
+    let regex = /\+(.*)(?=@)/gm;
+    const contact = req.body;
+    const strippedEmail = contact.email.replace(regex, '');
+
     try {
-      response = await db.collection('sourdough').insertOne({
+      await db.collection('sourdough').insertOne({
         ...req.body,
         uniqueEmail: strippedEmail,
         numberOfBreads: 0,
       });
+      request.body = {
+        contacts: [
+          {
+            email: contact.email,
+            first_name: contact.name,
+          },
+        ],
+      };
+      await client.request(request);
+      res.status(201).json({ ok: true });
     } catch (e) {
       if (e.code === 11000) {
         res.status(500).send("You've already been added to the list");
         return;
       }
-      response.status(500).end();
-      return;
+      res.status(500).end();
     }
-
-    res.status(201).json({ ok: true });
   } else {
     // Method not permitted
     res.status(405).end();
