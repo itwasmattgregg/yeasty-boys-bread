56560e4bb4c0630066cc3e0542c85e272b14bae7
Matt Gregg <mattdgregg@gmail.com>
2021-07-09 10:39:52 -0500

    Redesign of home page

---
+++ components/Nav.js
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import logo from "../images/logo.png";
+
+function Nav() {
+  return (
+    <div className="z-10 fixed p-5">
+      <Link to="/">
+        <Image src={logo} height={70} width={126} alt="Logo" />
+      </Link>
+    </div>
+  );
+}
+
+export default Nav;
