e9c42cc6d1cd46ed644875dc5920f78c2861db0a
Matt Gregg <mattdgregg@gmail.com>
2021-07-09 14:41:43 -0500

    Logo url

--- components/Layout.js
+++ components/Layout.js
@@ -15,7 +15,7 @@ function Layout(props) {
         <meta property="og:description" content="yeasty" />
         <meta
           property="og:image"
-          content={`https://yeastyboysbread.vercel.app/images/logo.png`}
+          content={`${process.env.VERCEL_URL}/images/logo.png`}
         />
         <title>Yeasty Boys Sourdough Bread Lottery</title>
         <link
