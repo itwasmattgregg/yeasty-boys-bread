6f7acd31ef47db15e22f2d4f64a4016887ccbc44
Matt Gregg <mattdgregg@gmail.com>
2021-07-09 14:08:29 -0500

    Layout changes

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,28 @@
 import Nav from "components/Nav";
+import Head from "next/head";
 
 function Layout(props) {
   return (
     <div className="relative overflow-hidden">
+      <Head>
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:creator" content="@itwasmattgregg" />
+        <meta
+          property="og:title"
+          content="Yeasty Boys Sourdough Bread Lottery"
+          key="title"
+        />
+        <meta property="og:description" content="yeasty" />
+        <meta
+          property="og:image"
+          content={`${window.location.origin}/images/logo.png`}
+        />
+        <title>Yeasty Boys Sourdough Bread Lottery</title>
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🍞</text></svg>"
+        />
+      </Head>
       <Nav />
       <div>{props.children}</div>
     </div>
--- pages/breadmachine.js
+++ pages/breadmachine.js
@@ -38,10 +38,6 @@ export default function BreadMachine() {
       <div className="container mx-auto">
         <Head>
           <title>Yeasty Boys Bread Machine</title>
-          <link
-            rel="icon"
-            href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🍞</text></svg>"
-          />
         </Head>
 
         <main className="mt-40">
