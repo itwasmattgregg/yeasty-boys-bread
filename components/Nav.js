50403089fad7985b9d1d757ec22169e8e0d843a4
Matt Gregg <mattdgregg@gmail.com>
2021-08-11 13:03:00 -0500

    Add shop code

--- components/Nav.js
+++ components/Nav.js
@@ -1,11 +1,9 @@
 import Image from "next/image";
 import Link from "next/link";
-import { useRouter } from "next/router";
 
 import logo from "../images/logo.png";
 
 function Nav() {
-  const router = useRouter();
   return (
     <>
       <div className="z-10 fixed p-6">
@@ -13,14 +11,19 @@ function Nav() {
           <Image src={logo} height={70} width={126} alt="Logo" />
         </Link>
       </div>
-      <div className="z-10 absolute right-10 top-10">
-        {router.pathname !== "/breadmachine" && (
-          <Link href="/breadmachine">
-            <a className="text-white font-bold text-shadow">
-              The Bread Machine
-            </a>
-          </Link>
-        )}
+      <div className="z-10 absolute right-10 top-10 grid gap-5 sm:flex sm:gap-10">
+        <Link href="/breadmachine">
+          <a className="font-bold flex p-3 items-center justify-center no-underline relative">
+            <div className="bg-white transform skew-x-12 h-full w-full absolute -z-10 border-black border"></div>
+            The Bread Machine
+          </a>
+        </Link>
+        <Link href="/shop">
+          <a className="font-bold flex p-3 items-center justify-center no-underline relative">
+            <div className="bg-white transform skew-x-12 h-full w-full absolute -z-10 border-black border"></div>
+            Shop
+          </a>
+        </Link>
       </div>
     </>
   );
