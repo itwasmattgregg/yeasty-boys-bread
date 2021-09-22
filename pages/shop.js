e39f4125855da167bd03f8079a46a662a3657b7f
Matt Gregg <mattdgregg@gmail.com>
2021-09-22 13:49:02 -0500

    Adjust div id

--- pages/shop.js
+++ pages/shop.js
@@ -9,7 +9,7 @@ const Shop = () => {
       </Head>
       <Layout>
         <div
-          id="product-component-1627505736920"
+          id="product-component-1631655763917"
           className="mt-40 min-h-screen px-5"
         ></div>
       </Layout>
