90c941a4c6c555b19a83e8dd1dd0d480decfacd3
Matt Gregg <mattdgregg@gmail.com>
2021-09-22 14:08:28 -0500

    Revert head change

--- components/Layout.js
+++ components/Layout.js
@@ -24,7 +24,6 @@ function Layout(props) {
           content={`https://${process.env.NEXT_PUBLIC_VERCEL_URL}/images/logo.png`}
         />
         <title>Yeasty Boys Sourdough Bread Lottery</title>
-        <script type="text/javascript" src="/shopifyButton.js" defer></script>
         <link
           rel="icon"
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🍞</text></svg>"
--- pages/shop.js
+++ pages/shop.js
@@ -1,8 +1,12 @@
 import Layout from "components/Layout";
+import Head from "next/head";
 
 const Shop = () => {
   return (
     <>
+      <Head>
+        <script type="text/javascript" src="/shopifyButton.js" defer></script>
+      </Head>
       <Layout>
         <div
           id="product-component-1631655763917"
