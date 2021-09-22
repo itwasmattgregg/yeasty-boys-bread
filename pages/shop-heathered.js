8c02d6f9308583781d39181dc15ec32a963a722b
Matt Gregg <mattdgregg@gmail.com>
2021-09-22 13:58:12 -0500

    Add heathered shirt page

---
+++ pages/shop-heathered.js
@@ -0,0 +1,23 @@
+import Layout from "components/Layout";
+import Head from "next/head";
+
+const Shop = () => {
+  return (
+    <>
+      <Head>
+        <script
+          type="text/javascript"
+          src="/shopifyButtonHeathered.js"
+        ></script>
+      </Head>
+      <Layout>
+        <div
+          id="product-component-1631655763917"
+          className="mt-40 min-h-screen px-5"
+        ></div>
+      </Layout>
+    </>
+  );
+};
+
+export default Shop;
