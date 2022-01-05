3db0aa66036f3af6d822582aba3db345231c2934
Matt Gregg <mattdgregg@gmail.com>
2022-01-04 20:35:13 -0600

    New shirt shop

---
+++ pages/shop-new-shirt.js
@@ -0,0 +1,18 @@
+import Layout from "components/Layout";
+import Script from "next/script";
+
+const Shop = () => {
+  return (
+    <>
+      <Script src="/shopifyNewShirtButton.js"></Script>
+      <Layout>
+        <div
+          id="product-component-1641349948140"
+          className="mt-40 min-h-screen px-5"
+        ></div>
+      </Layout>
+    </>
+  );
+};
+
+export default Shop;
