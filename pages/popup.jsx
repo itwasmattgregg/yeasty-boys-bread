685c8fc5521ae4a70754e9a978d1b1b9fca20125
Matt Gregg <matt.gregg@shopify.com>
2023-10-30 09:32:01 -0500

    popup test

---
+++ pages/popup.jsx
@@ -0,0 +1,17 @@
+export default function Popup() {
+  render(
+    <div>
+      <div className="container items-center max-w-3xl px-6 mx-auto my-10">
+        <h1 className="mb-12 text-2xl">What the fuck is this?</h1>
+        <p>
+          This is the official waiting list for Matt Gregg&lsquo;s sourdough. I
+          generally make one loaf to give away for free every week to friends
+          and family. If you live in the Twin Cities area of Minnesota, you know
+          me, and you love bread, you are eligible to enter this lottery. Please
+          fill out your name, email and full address below and I will notify you
+          if you&lsquo;ve won a loaf and have become a Breadwinner.
+        </p>
+      </div>
+    </div>
+  );
+}
