0eb9d8e1aac81810530e7e8549560b1f178e31ee
Matt Gregg <matt.gregg@shopify.com>
2023-10-30 09:40:23 -0500

    Fix errors

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,4 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import Nav from "../components/Nav";
 
 class MyDocument extends Document {
   render() {
--- pages/popup.jsx
+++ pages/popup.jsx
@@ -1,16 +1,17 @@
 export default function Popup() {
-  render(
-    <div>
-      <div className="container items-center max-w-3xl px-6 mx-auto my-10">
-        <h1 className="mb-12 text-2xl">What the fuck is this?</h1>
+  return (
+    <div className="container px-6 mx-auto mt-40">
+      <div className="mb-8">
+        <h1 className="relative mb-8 text-3xl font-bold leading-tight md:text-5xl md:leading-tight">
+          Popup menu
+        </h1>
         <p>
-          This is the official waiting list for Matt Gregg&lsquo;s sourdough. I
-          generally make one loaf to give away for free every week to friends
-          and family. If you live in the Twin Cities area of Minnesota, you know
-          me, and you love bread, you are eligible to enter this lottery. Please
-          fill out your name, email and full address below and I will notify you
-          if you&lsquo;ve won a loaf and have become a Breadwinner.
+          Welcome to the popup! Take a look at the menu below and have your
+          order ready to give to me at the window.
         </p>
+      </div>
+      <div className="mb-8">
+        <h2 className="mb-4 text-xl font-bold">Latte</h2>
       </div>
     </div>
   );
