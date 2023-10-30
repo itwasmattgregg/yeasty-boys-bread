71eba42747d0aeeb687d8822fcc2fdefc4063017
Matt Gregg <matt.gregg@shopify.com>
2023-10-30 10:09:51 -0500

    menu additions

--- pages/popup.jsx
+++ pages/popup.jsx
@@ -2,7 +2,7 @@ export default function Popup() {
   return (
     <div className="container px-6 mx-auto mt-40">
       <div className="mb-8">
-        <h1 className="relative mb-8 text-3xl font-bold leading-tight md:text-5xl md:leading-tight">
+        <h1 className="relative mb-4 text-3xl font-bold leading-tight md:text-5xl md:leading-tight">
           Popup menu
         </h1>
         <p>
@@ -12,6 +12,24 @@ export default function Popup() {
       </div>
       <div className="mb-8">
         <h2 className="mb-4 text-xl font-bold">Latte</h2>
+        <p>Options: oat milk | whole milk | pumpkin spice syrup | honey | iced</p>
+      </div>
+      <div className="mb-8">
+        <h2 className="mb-4 text-xl font-bold">Cortado or Cappuccino</h2>
+      </div>
+      <div className="mb-8">
+        <h2 className="mb-4 text-xl font-bold">Salted caramel hot cocoa</h2>
+      </div>
+      <div className="mb-8">
+        <h2 className="mb-4 text-xl font-bold">Hot apple cider</h2>
+      </div>
+      <div className="mb-8">
+        <h2 className="mb-4 text-xl font-bold">Tea</h2>
+        <p>Options: autumn spice | english breakfast | mint | berry</p>
+      </div>
+      <div className="mb-8">
+        <h2 className="mb-4 text-xl font-bold">Caramel roll</h2>
+        <p>A deliciously warm sweet roll with cinnamon and caramel (or is it carmel?)</p>
       </div>
     </div>
   );
